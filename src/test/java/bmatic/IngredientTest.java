bdb822ee5232e6613b9719cc94f29cd67fde5633
rDevelop <roblitvin@me.com>
2017-05-16 17:46:18 -0500

    Add an ingredient test

--- src/test/java/bmatic/IngredientTest.java
+++ src/test/java/bmatic/IngredientTest.java
@@ -1,7 +1,14 @@
 package bmatic;
 
+import bmatic.ingredient.UnitSugar;
+import org.junit.Test;
+
 /**
  * Created by rob on 5/16/17.
  */
 public class IngredientTest {
+    @Test
+    public void sugarCost() {
+        assert(new UnitSugar().getCost() == .25);
+    }
 }
